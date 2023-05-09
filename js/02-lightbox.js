6886c179d0a102ec4557c2c8bdb206178e7c6672
FredyBarajas <119268313+FredyBarajas@users.noreply.github.com>
2023-05-08 21:17:26 -0500

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-lightbox = new SimpleLightbox(".gallery a", {
+let lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
-  captionDelay: 300,
+  captionDelay: 200,
 });
