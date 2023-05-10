e90ea07f77da73ecc9ac27f9771262fcb96ceae7
FredyBarajas <119268313+FredyBarajas@users.noreply.github.com>
2023-05-09 19:49:47 -0500

    prevent default

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,15 +7,21 @@ const gallery = document.querySelector(".gallery");
 gallery.addEventListener("click", emergent);
 
 function emergent(event) {
+  event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
   let srcImage = event.target.src;
-  basicLightbox
-    .create(
-      `
+  const inst = basicLightbox.create(
+    `
 				<img width="1400" height="900" src="${srcImage}">
 			`
-    )
-    .show();
+  );
+  inst.show();
 }
+
+window.addEventListener("keyup", function (event) {
+  if (event.code === "27") {
+    inst.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
-
 let lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 200,
